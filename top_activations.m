bac0550e55d1a3f3dad34d2abadc07e4a7bea229
Shawn Jain <shawnjain.08@gmail.com>
2016-12-11 14:31:47 -0500

    top10 activation plot for all jpgs working

--- top_activations.m
+++ top_activations.m
@@ -1,52 +1,4 @@
 % do proj_setup.m
-%% Layer Indicies
-nlayers = size(net.layers);
-conv_indicies = [];
-for i = 1:nlayers(2)
-   if strcmp(net.layers{i}.type, 'conv') && (i < 14)
-       conv_indicies = [conv_indicies; i];
-   end
-end
-
-%% Get top N activations per layer having fed forward 1 image
-disp('Top Activations:')
-activation_determinants = struct;
-
-for conv_i = 1:numel(conv_indicies)
-    layer_i = conv_indicies(conv_i) + 1; % TODO: check indicies!
-    dets = [];
-    [vol_x vol_y nfilters] = size(res(layer_i).x);
-    for filter_i = 1:nfilters
-        A = res(layer_i).x(:,:,filter_i);
-        dets = [dets norm(A, 'fro')];
-    end
-    activation_determinants.(strcat('i', num2str(layer_i))) = dets;
-end
-
-%% Get top N activations per layer
-top10 = [];
-N = 10;
-for conv_i = 1:numel(conv_indicies)
-    layer_i = conv_indicies(conv_i) + 1;
-    nodes = getfield(activation_determinants, strcat('i', num2str(layer_i)));
-    [sorted_vals, sorted_idxs] = sort(nodes(:), 'descend');
-    fprintf('Layer %d: Top %d max activation indexes\n', layer_i, N);
-    for cur = 1:N
-        fprintf('%d\n', sorted_idxs(cur));
-    end
-    top10 = [top10; sorted_idxs(1:10)];
-end
-
-%% Plot max activations
-% x = layer_i
-% y = activation idx
-layer_i_expanded = [];
-for conv_i = 1:numel(conv_indicies)
-   layer_i_expanded = [layer_i_expanded; repmat(conv_indicies(conv_i) , N, 1)]; 
-end
-scatter(layer_i_expanded, top10);
-% TODO add an indicator of nfilters per layer
-
 %% Get activations for all imgs
 imgs = dir('/home/osboxes/Documents/top_weights/imgs/*.jpg');
 allres = struct;
@@ -58,3 +10,73 @@ for img = imgs'
    nojpg = strrep(img.name, '.jpg', '');
    allres.(strcat('i', nojpg)) = r;
 end
+
+%% Layer Indicies
+nlayers = size(net.layers);
+conv_indicies = [];
+for i = 1:nlayers(2)
+   if strcmp(net.layers{i}.type, 'conv') && (i < 14)
+       conv_indicies = [conv_indicies; i];
+   end
+end
+
+%% Get top N activations per layer having fed forward 1 image
+imgs_fields = fieldnames(allres);
+all_act_dets = struct;
+for img = 1:numel(imgs_fields)
+    res = allres.(imgs_fields{img});
+    fprintf('Calculating top activations for img %s:\n', imgs_fields{img});
+    activation_determinants = struct;
+
+    for conv_i = 1:numel(conv_indicies)
+        layer_i = conv_indicies(conv_i) + 1;
+        dets = [];
+        [vol_x vol_y nfilters] = size(res(layer_i).x);
+        for filter_i = 1:nfilters
+            A = res(layer_i).x(:,:,filter_i);
+            dets = [dets norm(A, 'fro')];
+        end
+        activation_determinants.(strcat('i', num2str(layer_i))) = dets;
+    end
+    all_act_dets.(imgs_fields{img}) = activation_determinants;
+end
+
+%% Get top N activations per layer
+all_act_dets_fields = fieldnames(all_act_dets);
+all_top10 = struct;
+for act_det = 1:numel(all_act_dets_fields)
+    activation_determinants = all_act_dets.(all_act_dets_fields{act_det});
+    top10 = [];
+    N = 10;
+    for conv_i = 1:numel(conv_indicies)
+        layer_i = conv_indicies(conv_i) + 1;
+        nodes = getfield(activation_determinants, strcat('i', num2str(layer_i)));
+        [sorted_vals, sorted_idxs] = sort(nodes(:), 'descend');
+        fprintf('Layer %d: Top %d max activation indexes\n', layer_i, N);
+        for cur = 1:N
+            fprintf('%d\n', sorted_idxs(cur));
+        end
+        top10 = [top10; sorted_idxs(1:10)];
+    end
+    all_top10.(all_act_dets_fields{act_det}) = top10;
+end
+
+%% Plot max activations
+% x = layer_i
+% y = activation idx
+
+all_top10_fields = fieldnames(all_top10);
+for top10_idx = 1:numel(all_top10_fields)
+    top10 = all_top10.(all_top10_fields{top10_idx});
+    layer_i_expanded = [];
+    for conv_i = 1:numel(conv_indicies)
+       layer_i_expanded = [layer_i_expanded; repmat(conv_indicies(conv_i) , N, 1)]; 
+    end
+    figure;
+    scatter(layer_i_expanded, top10);
+    fname = strrep(all_top10_fields{top10_idx}, '_', '\_');
+    title(strcat('Top10 Activation plot for :', ' ', fname));
+    % TODO add an indicator of nfilters per layer
+end
+%close all
+    
